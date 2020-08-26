8a1a6f1c08272ef5323b2df6475d11a8ed80c645
Jero <Jero@JERODSK>
2020-08-25 22:59:06 -0300

    Ejercicio 25082020

---
+++ 20200825/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numero = 0;
+            int intentos = 0;
+            Console.WriteLine("ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero!=7 && intentos<4)
+            
+            {
+                
+                Console.WriteLine("ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+                intentos++;
+            }
+
+            
+
+        }
+    }
+}
