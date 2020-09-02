af0ced68c151c2a44e1f9024224749e9f1d62dca
Jero <jeronimo.brandalise@davinci.edu.ar>
2020-09-01 22:39:48 -0300

    01092020

---
+++ 01092020/APP1/APP1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace APP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secreto = random.Next(10);
+
+            int num = 0;
+            for (int i=0; i<3;i++)
+            {
+
+                num = Inicio("ingrese un numero");
+
+
+                if (secreto == num)
+                {
+                    
+                    break;
+                }
+  
+            }
+            
+            
+            if(secreto == num)
+            {
+                Ganaste();
+
+            }
+            else
+            {
+                Perdiste(secreto);
+            }
+
+ 
+
+
+
+        }
+
+
+
+        static void Ganaste()
+        {
+            Console.WriteLine("Ganaste!");
+        }
+        static void Perdiste(int valor)
+        {
+            Console.WriteLine("perdiste, el número era: " + valor);
+        }
+
+        static int Inicio(string mensaje)
+        {
+            Console.WriteLine("ingrese un numero: ");
+            string ingreso = Console.ReadLine();
+            int num = int.Parse(ingreso);
+            return num;
+
+        }
+
+    }
+}
---
+++ 01092020/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fecha = IngresoFecha();
+            if(CalculoBi(fecha))
+            {
+
+                Console.WriteLine("Es año bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("No es año bisiesto");
+            }
+
+
+
+
+
+        }
+
+
+        static int IngresoFecha()
+        {
+
+            Console.WriteLine("ingrese año de nacimiento");
+            string valor = Console.ReadLine();
+            int fecha = int.Parse(valor);
+            return fecha;
+        }
+
+
+        static Boolean CalculoBi(int fecha)
+        {
+            if(fecha%4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+/*
+ armar un programa que le pida al usuario ingresar el año de la fecha de nacimiento y calcular si fue biciesto o no
+encapsular en un metodo el ingreso de datos pasando el texto a mostrarle al usuario
+y en otro metodo el calculo de si es biciesto devolviendo un valor booleano 
+ 
+ */
